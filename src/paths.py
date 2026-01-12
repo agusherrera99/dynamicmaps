dc73b16463f5cbf5e1c1c6a2fbaf6ade210849d6
agusherrera99 <agustinherrera.dev@gmail.com>
2026-01-12 13:55:43 -0300

    Add ProjectPath.parquets_path; Update ProjectPath.surtidores_departamentos_path value to use parquets_path dir and .parquet extension

--- src/paths.py
+++ src/paths.py
@@ -8,11 +8,12 @@ class ProjectPath:
 
         self.csvs_path: PosixPath = self.root_path / "csvs"
         self.geopackages_path: PosixPath = self.root_path / "geopackages"
+        self.parquets_path: PosixPath = self.root_path / "parquets"
 
         self.surtidores_path: PosixPath = self.csvs_path / "surtidores_buenos_aires.csv"
         self.departamentos_path: PosixPath = self.geopackages_path / "departamentos_buenos_aires.geojson"
 
-        self.surtidores_departamentos_path: PosixPath = self.geopackages_path / "surtidores_departamentos_buenos_aires.geojson"
+        self.surtidores_departamentos_path: PosixPath = self.parquets_path / "surtidores_departamentos_buenos_aires.parquet"
 
     def get_surtidores_path(self) -> PosixPath:
         return self.surtidores_path
